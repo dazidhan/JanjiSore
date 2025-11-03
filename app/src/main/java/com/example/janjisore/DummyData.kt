14c2482c0bb285d298c01cd61d55e8df52abaec6
Dhaffa Zikrullah Ramadhan <dhaffazr@gmail.com>
2025-11-03 13:06:10 +0700

    tambah data dummy

--- app/src/main/java/com/example/janjisore/DummyData.kt
+++ app/src/main/java/com/example/janjisore/DummyData.kt
@@ -50,6 +50,126 @@ object DummyData {
                 27000,
                 "Cold",
                 R.drawable.iced_taro_milk
+            ),
+            Drink(
+                "Cappuccino",
+                "Kopi",
+                "Espresso dengan steamed milk dan busa susu.",
+                23000,
+                "Hot",
+                R.drawable.cappuccino
+            ),
+            Drink(
+                "Kopi Tubruk",
+                "Kopi",
+                "Kopi tradisional Indonesia dengan gula aren.",
+                15000,
+                "Hot",
+                R.drawable.kopi_tubruk
+            ),
+            Drink(
+                "Espresso",
+                "Kopi",
+                "Shot kopi murni yang kuat dan pekat.",
+                12000,
+                "Hot",
+                R.drawable.espresso
+            ),
+            Drink(
+                "Iced Caramel Macchiato",
+                "Kopi",
+                "Espresso dengan susu, vanilla, dan caramel.",
+                28000,
+                "Cold",
+                R.drawable.ice_caramel_macchiato
+            ),
+            Drink(
+                "Iced Vanilla Latte",
+                "Kopi",
+                "Latte dingin dengan rasa vanilla yang lembut.",
+                26000,
+                "Cold",
+                R.drawable.ice_vanilla_latte
+            ),
+            Drink(
+                "Cold Brew Coffee",
+                "Kopi",
+                "Kopi yang diseduh dingin selama 12 jam.",
+                20000,
+                "Cold",
+                R.drawable.cold_brew_coffee
+            ),
+            Drink(
+                "Hot Chocolate",
+                "Non-Kopi",
+                "Coklat panas premium dengan whipped cream.",
+                20000,
+                "Hot",
+                R.drawable.hot_chocolate
+            ),
+            Drink(
+                "Taro Latte",
+                "Non-Kopi",
+                "Minuman ubi ungu dengan rasa yang unik.",
+                29000,
+                "Hot",
+                R.drawable.taro_latte
+            ),
+            Drink(
+                "Red Velvet Latte",
+                "Non-Kopi",
+                "Perpaduan coklat dan red velvet yang manis.",
+                32000,
+                "Hot",
+                R.drawable.red_velvet_latte
+            ),
+            Drink(
+                "Chai Latte",
+                "Non-Kopi",
+                "Teh rempah India dengan susu dan madu.",
+                27000,
+                "Hot",
+                R.drawable.chai_latte
+            ),
+            Drink(
+                "Iced Matcha Latte",
+                "Non-Kopi",
+                "Matcha segar dengan susu dingin dan es.",
+                32000,
+                "Cold",
+                R.drawable.iced_matcha_latte
+            ),
+            Drink(
+                "Strawberry Smoothie",
+                "Non-Kopi",
+                "Smoothie strawberry segar dengan yogurt.",
+                25000,
+                "Cold",
+                R.drawable.strawberry_smoothie
+            ),
+            Drink(
+                "Mango Float",
+                "Non-Kopi",
+                "Blended mango dengan whipped cream.",
+                28000,
+                "Cold",
+                R.drawable.mango_float
+            ),
+            Drink(
+                "Thai Tea",
+                "Non-Kopi",
+                "Teh Thailand dengan susu dan gula aren.",
+                23000,
+                "Cold",
+                R.drawable.thai_tea
+            ),
+            Drink(
+                "Lemonade Mint",
+                "Non-Kopi",
+                "Lemonade segar dengan daun mint.",
+                18000,
+                "Cold",
+                R.drawable.lemonade_mint
             )
         )
     }
